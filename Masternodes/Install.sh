ba08329ddcdbb43644ce4677987dddab56a87ef5
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-19 21:06:55 +0800

    Update Install.sh

--- Masternodes/Install.sh
+++ Masternodes/Install.sh
@@ -9,25 +9,10 @@ echo "Installing unzip..."
 sudo apt-get install unzip -y
 echo "Unzipping latest zip..."
 cd && cd Downloads
-sudo unzip -q encrypt-1.3.0.0-linux.zip -d /usr/local/bin/
+sudo unzip encrypt-1.3.0.0-linux.zip -d /usr/local/bin/
 cd && sudo chmod +x /usr/local/bin/encrypt*
 echo "Creating .encrypt directory..."
 mkdir ~/.encrypt
-cd ~/.encrypt
-echo "Editing encrypt.conf..."
-echo rpcuser=someuserhere
-echo rpcpassword=somepasswordhere
-echo rpcallowip=127.0.0.1
-echo server=1
-echo daemon=1
-echo listen=1
-echo staking=0
-echo logtimestamps=1
-echo maxconnections=256
-echo masternode=1
-echo externalip=
-echo masternodeprivkey=
-vi encrypt.conf
 echo "Setting up and enabling fail2ban..."
 sudo apt-get install fail2ban -y
 sudo ufw allow ssh
@@ -36,7 +21,7 @@ sudo ufw allow 2121
 sudo ufw enable
 echo "Launching encryptd..."
 cd && cd /usr/local/bin
-echo ./encryptd -daemon
+echo ./encryptd --daemon
 echo "Cleaning up..."
 cd
 rm -rf encrypt-1.3.0.0-linux.zip
