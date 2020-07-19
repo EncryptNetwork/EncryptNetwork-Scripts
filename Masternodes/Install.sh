4f8db98c7788e5dc1b2889462d3b241a98c6042b
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-19 21:41:38 +0800

    Update Install.sh

--- Masternodes/Install.sh
+++ Masternodes/Install.sh
@@ -13,21 +13,6 @@ cd && sudo chmod +x /usr/local/bin/encrypt*
 echo "Creating .encrypt directory..."
 mkdir ~/.encrypt
 cd ~/.encrypt
-echo "Editing encrypt.conf..."
-cat >> encrypt.conf <<'EOF'
-rpcuser=someuserhere>> encrypt.conf
-rpcpassword=somepasswordhere>> encrypt.conf
-rpcallowip=127.0.0.1 >> encrypt.conf
-server=1 >> encrypt.conf
-daemon=1 >> encrypt.conf
-listen=1 >> encrypt.conf
-staking=0 >> encrypt.conf
-logtimestamps=1 >> encrypt.conf
-maxconnections=256 >> encrypt.conf
-masternode=1 >> encrypt.conf
-externalip= >> encrypt.conf
-masternodeprivkey= >> encrypt.conf
-EOF
 echo "Setting up and enabling fail2ban..."
 sudo apt-get install fail2ban -y
 sudo ufw allow ssh
@@ -36,7 +21,7 @@ sudo ufw allow 2121
 sudo ufw enable
 echo "Launching encryptd..."
 cd && cd /usr/local/bin
-./encryptd --daemon
+encryptd -daemon
 echo "Cleaning up..."
 cd
 rm -rf encrypt-1.3.0.0-linux.zip
