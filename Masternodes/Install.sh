693e3aef5a27b53d37fb8988e7bbd7ca275ddd60
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-19 21:14:26 +0800

    Update Install.sh

--- Masternodes/Install.sh
+++ Masternodes/Install.sh
@@ -8,7 +8,6 @@ wget -N https://github.com/EncryptNetwork/EncryptNetwork/releases/download/1.3.0
 echo "Installing unzip..."
 sudo apt-get install unzip -y
 echo "Unzipping latest zip..."
-cd && cd Downloads
 sudo unzip encrypt-1.3.0.0-linux.zip -d /usr/local/bin/
 cd && sudo chmod +x /usr/local/bin/encrypt*
 echo "Creating .encrypt directory..."
@@ -21,7 +20,7 @@ sudo ufw allow 2121
 sudo ufw enable
 echo "Launching encryptd..."
 cd && cd /usr/local/bin
-echo ./encryptd --daemon
+./encryptd --daemon
 echo "Cleaning up..."
 cd
 rm -rf encrypt-1.3.0.0-linux.zip
