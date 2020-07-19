49c00f38e64ee4754c342b23e482ebc4717cb5ac
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-19 10:05:27 +0800

    Initial Commit

---
+++ Masternodes/README.md
@@ -0,0 +1,7 @@
+# Masternode Scripts
+ Various scripts for setting up masternodes
+
+ Install.sh - For a fresh install of the latest encryptd
+ InstallIPv6.sh - For a fresh install of the latest encryptd on IPv6 [work in progress]
+ Update.sh - To update an already installed version of encryptd
+ setup_multi.sh - To multi-install and configure your ENCRYPT masternodes
---
+++ Masternodes/Update.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+clear
+echo "Starting ENCRYPT Masternode update script"
+echo "Stopping encryptd..."
+encrypt-cli stop
+echo "Downloading EncryptNetwork update..."
+wget -N https://github.com/EncryptNetwork/EncryptNetwork/releases/download/1.3.0.0/encrypt-1.3.0.0-linux.zip
+echo "Installing unzip..."
+sudo apt-get install unzip -y
+echo "Extracting update..."
+sudo unzip -jo encrypt-1.3.0.0-linux.zip -d /usr/local/bin
+echo "Set permissions on files..."
+sudo chmod +x /usr/local/bin/encrypt*
+echo "Running encryptd..."
+encryptd -daemon
+echo "Cleaning up..."
+cd
+rm -rf encrypt-1.3.0.0-li
+echo "Update completed!"
