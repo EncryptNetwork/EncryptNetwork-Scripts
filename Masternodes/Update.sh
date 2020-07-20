513175f4c24bd65acfbfea1958eed37bac4badee
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-20 17:01:39 +0800

    Update Update.sh

--- Masternodes/Update.sh
+++ Masternodes/Update.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 clear
 echo "Starting ENCRYPT Masternode update script"
+cd && cd /usr/local/bin
 echo "Stopping encryptd..."
 encrypt-cli stop
+cd && cd /enscripts/Masternodes
 echo "Downloading EncryptNetwork update..."
 wget -N https://github.com/EncryptNetwork/EncryptNetwork/releases/download/1.3.0.0/encrypt-1.3.0.0-linux.zip
 echo "Installing unzip..."
@@ -12,8 +14,9 @@ sudo unzip encrypt-1.3.0.0-linux.zip -d /usr/local/bin/
 echo "Set permissions on files..."
 cd && sudo chmod +x /usr/local/bin/encrypt*
 echo "Running encryptd..."
+cd && cd /usr/local/bin
 encryptd -daemon
 echo "Cleaning up..."
-cd
+cd && cd /enscripts/Masternodes
 rm -rf encrypt-1.3.0.0-linux.zip
 echo "ENCRYPT Update completed!"
